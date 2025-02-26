46edce3f0e60bae4b6ff62a16ba152be563d98a9
Shamir Duran <shamirduran15@gmail.com>
2025-02-26 12:32:12 -0500

    project preparation

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const { port, prefix } = require('./src/configs/env.config');
-const { logger } = require('./src/utils/logs.util');
-const errorHandlerMiddleware = require('./src/middlewares/error.middleware');
+const { port, prefix } = require("./src/configs/env.config");
 
 // Express
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const morgan = require("morgan");
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
+app.use(morgan("dev"));
 
 // Routes
-app.get(`/${prefix}`, (req, res) => res.json({ message: 'ok' }));
-app.use(`/${prefix}/test`, require('./src/routes/test.route'));
-
-// Error handler
-app.use(errorHandlerMiddleware);
+app.get(`/${prefix}`, (req, res) => res.json({ message: "ok" }));
+app.use(`/${prefix}/test`, require("./src/routes/test.route"));
 
 app.listen(port, () => {
-  logger('info', `Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
